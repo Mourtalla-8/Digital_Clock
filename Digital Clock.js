a7fc1c101968b9414d41a6b380eeadcf05c58597
TANJIRO-8 <touremourtalla841@gmail.com>
2023-10-11 16:47:38 +0000

    ajout des fichiers css et js

---
+++ Digital Clock.js
@@ -0,0 +1,26 @@
+function clock(){
+                
+    var monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ]; 
+    var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+    var today = new Date();
+
+    document.getElementById('Date').innerHTML = (dayNames[today.getDay()] + " " + 
+    today.getDate() + ' ' + monthNames[today.getMonth()] + ' ' +today.getFullYear());
+
+    
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var day = h<11 ? 'AM': 'PM';
+
+    h = h<10? '0'+h: h;
+    m = m<10? '0'+m: m;
+    s = s<10? '0'+s: s;               
+
+    document.getElementById('hours').innerHTML = h;
+    document.getElementById('min').innerHTML = m;
+    document.getElementById('sec').innerHTML = s;
+
+}var inter = setInterval(clock,400);
